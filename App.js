289c20e311d92c38f4c69a7b52ca20e45d76a535
Shamli-237 <164298065+Shamli-237@users.noreply.github.com>
2024-03-21 19:22:11 +0530

    Add files via upload
    code upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import Background_Service from './Background_Service';
+
+
+const App = () => {
+  return (
+   <Background_Service></Background_Service>
+
+  )
+}
+export default App;
